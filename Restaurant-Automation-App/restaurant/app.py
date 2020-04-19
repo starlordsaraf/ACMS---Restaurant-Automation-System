047f70d4bfc3b5866772db02c3f89bc002f845f4
mainakisaraf <mainakisaraf@gmail.com>
2020-04-19 19:22:48 +0530

    added gsi for menu,tables impl. tables api

--- Restaurant-Automation-App/restaurant/app.py
+++ Restaurant-Automation-App/restaurant/app.py
@@ -1,7 +1,10 @@
 import json
 import boto3
 from flask_lambda import FlaskLambda
+from flask import jsonify
 from flask import request
+from flask import Response
+from boto3.dynamodb.conditions import Key
 # import requests
 
 app = FlaskLambda(__name__)
@@ -10,34 +13,6 @@ table = ddb.Table('restaurants')
 
 @app.route('/')
 def index():
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     data = {
         "message": "Welcome to Restaurant Automation"
@@ -61,6 +36,18 @@ def put_or_list_restuarants():
             200,
             {'Content-Type': "application/json"}
         )
+
+
+
+@app.route('/restaurants/seating/<string:resid>', methods=['GET'])
+def getseatingchart(resid):
+    response = table.query(IndexName="seatingGSI",KeyConditionExpression=Key("Resid").eq(resid))
+    if(response['Items'][0]["Seating"]==[]):
+        return(json.dumps("Seating Chart not updated"),204,{'Content-Type': "application/json"})
+    else:
+        return(jsonify(response['Items'][0]['Seating']),200, {'Content-Type': "application/json"})
+
+
 
 @app.route('/restaurants/menu/<string:resid>', methods=['GET','POST'])
 def menu(resid):
