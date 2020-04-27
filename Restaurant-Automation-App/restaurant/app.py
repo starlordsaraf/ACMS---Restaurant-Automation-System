e115e72c15be7fee02964fc2706903403fdb0f31
Archana <arch.2421@gmail.com>
2020-04-27 13:08:26 +0530

    Login and Signup added

--- Restaurant-Automation-App/restaurant/app.py
+++ Restaurant-Automation-App/restaurant/app.py
@@ -10,6 +10,7 @@ from boto3.dynamodb.conditions import Key
 app = FlaskLambda(__name__)
 ddb = boto3.resource("dynamodb")
 table = ddb.Table('restaurants')
+logintable = ddb.Table('logintable')
 
 @app.route('/')
 def index():
@@ -63,3 +64,50 @@ def menu(resid):
                 )
             else:
                 return(json.dumps("Restaurant doesnt exist"),200,{'Content-Type': "application/json"})
+
+# {"Resname":"Pizza hut","Resaddr":"Banashkari,98/4","Resnum":"23316745","Resid":"1","Username":"PizHut","Password":"1234"}
+@app.route('/restaurants/signup', methods=['POST'])
+def signup():
+    req=request.get_json()
+    uname=req["Username"]
+    pwd=req["Password"]
+    resid=req["Resid"]
+
+    name=req["Resname"]
+    num=req["Resnum"]
+    addr=req["Resaddr"]
+    
+    req1=jsonify({"Resid":resid,"Username":uname,"Password":pwd})
+    req2=jsonify({"Resid":resid,"Menu":[],"Offers":[],"Seating":[],"Resname":name,"Resnum":num,"Resaddr":addr})
+
+    logintable.put_item(Item={"Resid":resid,"Username":uname,"Password":pwd})
+    table.put_item(Item={"Resid":resid,"Menu":[],"Offers":[],"Seating":[],"Resname":name,"Resnum":num,"Resaddr":addr})
+    return(
+        json.dumps({"message": "entry made"}),
+        200,
+        {'Content-Type': "application/json"}
+    )
+    
+#{"Resid":"1","Username":"PizHut","Password":"1234"}
+@app.route('/restaurants/login', methods=['POST'])
+def login():
+    req=request.get_json()
+    resid=req["Resid"]
+    uname=req["Username"]
+    pwd=req["Password"]
+    
+    res = logintable.scan()['Items']
+    for restaurants in res:
+        if(restaurants['Username']==uname):
+            chk=1
+            if restaurants['Password']==pwd:
+                return(
+                    json.dumps({"message": "login successful"}),
+                    200,
+                    {'Content-Type': "application/json"}
+                )        
+    return(
+        json.dumps({"message": "Invalid credentials!"}),
+        200,
+        {'Content-Type': "application/json"}
+    )
