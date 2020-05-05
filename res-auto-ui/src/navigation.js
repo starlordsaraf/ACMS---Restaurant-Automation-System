cbcabd14125bbef03e557b15fd9c5fb163353966
Anusha Rao <53226563+AnushaSRao@users.noreply.github.com>
2020-05-05 18:38:38 +0530

    Menu frontend-Updated

---
+++ res-auto-ui/src/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import { NavLink } from 'react-router-dom';
+ 
+const Navigation = () => {
+    return (
+       <div>
+          <NavLink to="/menuDisplay">Menu</NavLink> &emsp;&emsp;&emsp;
+          <NavLink to="/Dish">Add/Update</NavLink>
+       </div>
+    );
+}
+ 
+export default Navigation;
