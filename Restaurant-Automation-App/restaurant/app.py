cc4c0edfb67e268afaf1f32a51bd350e0878f0aa
sanjanam1998 <36978563+sanjanam1998@users.noreply.github.com>
2020-05-05 23:19:42 +0530

    final menu api's

--- Restaurant-Automation-App/restaurant/app.py
+++ Restaurant-Automation-App/restaurant/app.py
@@ -92,19 +92,55 @@ def del_dish(resid):
         {'Content-Type': "application/json"}
     )
 
-@app.route('/restaurants/menu/<string:resid>', methods=['DELETE'])
-def del_menu(resid):
-    #this will be implemented later
-    return (
-        json.dumps({"message": "Menu deleted"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
-
 @app.route('/restaurants/menu/dish/<string:resid>', methods=['POST'])
 def update_dish(resid):
-    #this will be implemented later
     dish = request.json
+    dname = dish['dishname']
+    ingredients = dish['ingredients']
+    price = dish['price']
+    quantity = dish['quantity']
+    res = table.query(
+        KeyConditionExpression=Key('ResId').eq(resid) & Key('RecordId').begins_with("DISH_DETAIL"),
+        FilterExpression="Dishname = :d",
+        ExpressionAttributeValues={
+            ':d': dname
+        },
+        ProjectionExpression="RecordId"
+    )
+    recordid = res['Items'][0]['RecordId']
+    if(quantity):
+        table.update_item(
+            Key={
+                'ResId': resid,
+                'RecordId': recordid
+            },
+            UpdateExpression="set quant = :q",
+            ExpressionAttributeValues={
+                ':q': quantity
+            }
+        )
+    if(ingredients):
+        table.update_item(
+            Key={
+                'ResId': resid,
+                'RecordId': recordid
+            },
+            UpdateExpression="set ingredients = :i",
+            ExpressionAttributeValues={
+                ':i': ingredients
+            }
+        )
+    if(price):
+        table.update_item(
+            Key={
+                'ResId': resid,
+                'RecordId': recordid
+            },
+            UpdateExpression="set price = :p",
+            ExpressionAttributeValues={
+                ':p': price
+            }
+        )
     return (
         json.dumps({"message": "Dish updated"}),
         200,
@@ -118,16 +154,13 @@ def signup():
     uname=req["Username"]
     pwd=req["Password"]
     resid=req["Resid"]
-
+    
     name=req["Resname"]
     num=req["Resnum"]
     addr=req["Resaddr"]
     
-    req1=jsonify({"Resid":resid,"Username":uname,"Password":pwd})
-    req2=jsonify({"Resid":resid,"Menu":[],"Offers":[],"Seating":[],"Resname":name,"Resnum":num,"Resaddr":addr})
-
     logintable.put_item(Item={"Resid":resid,"Username":uname,"Password":pwd})
-    table.put_item(Item={"Resid":resid,"Menu":[],"Offers":[],"Seating":[],"Resname":name,"Resnum":num,"Resaddr":addr})
+    table.put_item(Item={"ResId":resid,"RecordId":"RES_DETAIL","Resname":name,"Resnum":num,"Resaddr":addr})
     return(
         json.dumps({"message": "entry made"}),
         200,
@@ -157,127 +190,7 @@ def login():
         200,
         {'Content-Type': "application/json"}
     )
-=======
-import json
-import boto3
-from flask_lambda import FlaskLambda
-from flask import jsonify
-from flask import request
-from flask import Response
-from boto3.dynamodb.conditions import Key
-# import requests
 
-app = FlaskLambda(__name__)
-ddb = boto3.resource("dynamodb")
-table = ddb.Table('restaurant-table')
-logintable = ddb.Table('logintable')
-
-@app.route('/')
-def index():
-    data = {
-        "message": "Welcome to Restaurant Automation"
-    }
-    return json.dumps(data)
-
-
-@app.route('/restaurants', methods=['GET','POST'])
-def put_or_list_restuarants():
-    if(request.method == 'GET'):
-        res = table.scan()['Items']
-        return(
-            json.dumps(res),
-            200,
-            {'Content-Type': "application/json"}
-        )
-    else:
-        table.put_item(Item=request.json)
-        return(
-            json.dumps({"message": "entry made"}),
-            200,
-            {'Content-Type': "application/json"}
-        )
-
-
-#API to get all seats for seating chart
-@app.route('/restaurants/seating/<string:resid>', methods=['GET'])
-def getseatingchart(resid):
-    response = table.query(KeyConditionExpression=Key("ResId").eq(resid) & Key('RecordId').begins_with("TABLE_DETAIL"))
-    if(response['Items'] == []):
-        return(json.dumps("Seating Chart not updated"),200,{'Content-Type': "application/json"})
-    else:
-        return(jsonify(response['Items']),200, {'Content-Type': "application/json"})
-
-
-
-@app.route('/restaurants/menu/<string:resid>', methods=['GET'])
-def get_menu(resid):
-    response = table.query(KeyConditionExpression=Key("ResId").eq(resid) & Key('RecordId').begins_with("DISH_DETAIL"))
-    menu = response['Items']
-    if (menu):
-        return (
-            jsonify(menu),
-            200,
-            {'Content-Type': "application/json"}
-        )
-    return (json.dumps("Restaurant doesn't exist"), 200, {'Content-Type': "application/json"})
-
-@app.route('/restaurants/menu/<string:resid>', methods=['PUT'])
-def add_dish(resid):
-    dish = request.json
-    dname = dish['dishname']
-    category = dish['category']
-    ingredients = dish['ingredients']
-    price = dish['price']
-    quantity = dish['quantity']
-    recordid = dish['did']
-    table.put_item(Item={"ResId": resid, "RecordId": recordid, "Dishname": dname, "category": category, "ingredients": ingredients,
-                         "quant": quantity, "price": price})
-    return (
-        json.dumps({"message": "Dish added"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
-
-@app.route('/restaurants/menu/<string:resid>', methods=['DELETE'])
-def del_dish(resid):
-    dish = request.json
-    dname = dish["dname"]
-    table.delete_item(
-        Key={"ResId":resid},
-        ConditionExpression= "#Dishname = :dname",
-        ExpressionAttributeNames={"#Dishname": "Dishname"},
-        ExpressionAttributeValues={":dname": dname}
-    )
-    return (
-        json.dumps({"message": "Dish deleted"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
-
-@app.route('/restaurants/menu/<string:resid>', methods=['POST'])
-def update_dish(resid):
-    dish = request.json
-    return (
-        json.dumps({"message": "Dish updated"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
-
-
-@app.route('/restaurants/menu/<string:resid>', methods=['GET','POST'])
-def menu(resid):
-    if(request.method == 'GET'):
-        res = table.scan()['Items']
-        for restaurants in res:
-            if(restaurants['Resid']==resid):
-                menu=restaurants['Menu']
-                return(
-                    json.dumps(menu),
-                    200,
-                    {'Content-Type': "application/json"}
-                )
-            else:
-                return(json.dumps("Restaurant doesnt exist"),200,{'Content-Type': "application/json"})
 
 @app.route('/restaurants/update', methods=['POST'])
 def updateres():
@@ -342,49 +255,4 @@ def deleteres():
             200,
             {'Content-Type': "application/json"}
         )
-    
-# {"Resname":"Pizza hut","Resaddr":"Banashkari,98/4","Resnum":"23316745","Resid":"1","Username":"PizHut","Password":"1234"}
-@app.route('/restaurants/signup', methods=['POST'])
-def signup():
-    req=request.get_json()
-    uname=req["Username"]
-    pwd=req["Password"]
-    resid=req["Resid"]
-    
-    name=req["Resname"]
-    num=req["Resnum"]
-    addr=req["Resaddr"]
-    
-    logintable.put_item(Item={"Resid":resid,"Username":uname,"Password":pwd})
-    table.put_item(Item={"ResId":resid,"RecordId":"RES_DETAIL","Resname":name,"Resnum":num,"Resaddr":addr})
-    return(
-        json.dumps({"message": "entry made"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
-
-    
-#{"Resid":"1","Username":"PizHut","Password":"1234"}
-@app.route('/restaurants/login', methods=['POST'])
-def login():
-    req=request.get_json()
-    resid=req["Resid"]
-    uname=req["Username"]
-    pwd=req["Password"]
-    
-    res = logintable.scan()['Items']
-    for restaurants in res:
-        if(restaurants['Username']==uname):
-            chk=1
-            if restaurants['Password']==pwd:
-                return(
-                    json.dumps({"message": "login successful"}),
-                    200,
-                    {'Content-Type': "application/json"}
-                )        
-    return(
-        json.dumps({"message": "Invalid credentials!"}),
-        200,
-        {'Content-Type': "application/json"}
-    )
 
